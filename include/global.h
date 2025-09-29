6007961edff8fc62c8d82ba116e521e312ed2f87
resetes12 <diploporigon@gmail.com>
2025-09-29 23:22:09 +0200

    Edited, improved and tested all the challenges from Modern Emerald

--- include/global.h
+++ include/global.h
@@ -1172,6 +1172,7 @@ struct SaveBlock1
         u8 tx_Mode_New_Legendaries:1;
         u8 tx_Challenges_Expensive:3;
         u8 tx_Difficulty_EscapeRopeDig:1;
+        u8 tx_Mode_Encounters:2; //unused in HnS
         u8 tx_Features_FrontierBans:1;
         u8 tx_Difficulty_HardExp:1;
         u8 tx_Mode_TypeEffectiveness:1;
