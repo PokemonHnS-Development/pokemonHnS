1c3de2f8c88134948ebbb793424ff36b98ab44c9
resetes12 <diploporigon@gmail.com>
2025-10-03 19:26:09 +0200

    Added gym leaders, Red, Leaf, Lance and Steven as Frontier Trainers

--- include/battle_tower.h
+++ include/battle_tower.h
@@ -33,10 +33,10 @@ struct FacilityMon
     u8 nature;
 };
 
-extern const u8 gTowerMaleFacilityClasses[30];
-extern const u8 gTowerMaleTrainerGfxIds[30];
-extern const u8 gTowerFemaleFacilityClasses[20];
-extern const u8 gTowerFemaleTrainerGfxIds[20];
+extern const u8 gTowerMaleFacilityClasses[42];
+extern const u8 gTowerMaleTrainerGfxIds[42];
+extern const u8 gTowerFemaleFacilityClasses[28];
+extern const u8 gTowerFemaleTrainerGfxIds[28];
 extern const u16 gBattleFrontierHeldItems[];
 extern const struct FacilityMon gBattleFrontierMons[];
 extern const struct FacilityMon gBattleFrontierMonsSplit[];
