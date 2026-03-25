7ebca896898289b352ac9559d901532930c8a2af
Andrew Strauss <astrauss11@gmail.com>
2026-03-24 20:00:24 -0400

    Fix bug catching contest white out ending hardcore nuzlock
    Signed-off-by: Andrew Strauss <astrauss11@gmail.com>

--- src/overworld.c
+++ src/overworld.c
@@ -1993,11 +1993,6 @@ void CB2_BugContestWhiteOut(void){
     {
         FieldClearVBlankHBlankCallbacks();
         StopMapMusic();
-        if (gSaveBlock1Ptr->tx_Challenges_NuzlockeHardcore && !FlagGet(FLAG_DEFEATED_RED)) //tx_randomizer_and_challenges
-        {
-            ClearSaveData();
-            DoSoftReset();
-        }
         ResetSafariZoneFlag_();
 
         ResetInitialPlayerAvatarState();
