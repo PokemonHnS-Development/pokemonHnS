9a53eeb90e887879833a0fd0cb1ddb146422f863
Skeletonkey36 <isaaclien@protonmail.com>
2026-03-31 14:00:19 -0500

    fix: accidentally put a flag with the vars causing OOB issues when accessing the flag for the GBS system

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -709,8 +709,8 @@
 #define FLAG_SHINY_STARTER_3  0x296 // Flag for shiny starters, Totodile
 #define FLAG_FORCE_SHINY   0x297 // Needed for shiny starters, not used anywhere else
 #define FLAG_TEMPORALY_DISABLE_STATIC_RANDOMIZER  0x298
-#define FLAG_MOM_HAS_GIFT  0x299 // Unused Flag
-#define FLAG_UNUSED_0x29A  0x29A // Unused Flag
+#define FLAG_MOM_HAS_GIFT  0x299 // Used for Mom's Saving system
+#define FLAG_SYS_GBS_ENABLED  0x29A // GB Player Active Flag
 #define FLAG_UNUSED_0x29B  0x29B // Unused Flag
 #define FLAG_UNUSED_0x29C  0x29C // Unused Flag
 #define FLAG_UNUSED_0x29D  0x29D // Unused Flag
